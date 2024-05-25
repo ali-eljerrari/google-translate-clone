d9578dfb61cc0d889c97b7d14491fa1d736ccd07
Ali Eljerrari <contact@coding-dynasty.xyz>
2024-05-25 23:34:18 +0100

    aded translation api

--- components/TranslationForm.tsx
+++ components/TranslationForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import SubmitButton from './SubmitButton';
+import { useState } from 'react';
 import { Textarea } from './ui/textarea';
 import {
   Select,
@@ -9,18 +9,74 @@ import {
   SelectItem,
   SelectLabel,
   SelectTrigger,
-  SelectValue
+  SelectValue,
 } from '@/components/ui/select';
+import axios from 'axios';
+import { Button } from './ui/button';
+
+const subscriptionKey = process.env.NEXT_PUBLIC_AZURE_API_KEY;
+const endpoint = process.env.NEXT_PUBLIC_AZURE_ENDPOINT;
+const location = process.env.NEXT_PUBLIC_AZURE_DEPLOYMENT_NAME;
 
 function TranslationForm({ languages }: { languages: any }) {
+  const [text, setText] = useState('');
+  const [to, setTo] = useState('ar');
+  const [translatedText, setTranslatedText] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    if (!text || !to) return;
+
+    try {
+      setLoading(true);
+      const response = await axios({
+        baseURL: endpoint,
+        url: '/translate',
+        method: 'post',
+        headers: {
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+          'Ocp-Apim-Subscription-Region': location,
+          'Content-type': 'application/json',
+        },
+        params: {
+          'api-version': '3.0',
+          to: to,
+        },
+        data: [
+          {
+            text: text,
+          },
+        ],
+        responseType: 'json',
+      });
+
+      setTranslatedText(response.data[0].translations[0].text);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  const translate = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  const handleValueChange = (value: string) => {
+    setTo(value);
+    setTranslatedText('');
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={translate}>
         <div className='flex flex-col space-y-2 lg:flex-row lg:space-y-0 lg:space-x-2'>
           <div className='flex-1 space-y-2'>
             <Select
               name='inputLanguage'
-              defaultValue='auto'>
+              defaultValue='auto'
+            >
               <SelectTrigger className='w-[280px] border-none text-blue-500 font-bold'>
                 <SelectValue placeholder='Select a language' />
               </SelectTrigger>
@@ -31,7 +87,8 @@ function TranslationForm({ languages }: { languages: any }) {
 
                   <SelectItem
                     key='auto'
-                    value='auto'>
+                    value='auto'
+                  >
                     Auto-Detection
                   </SelectItem>
                 </SelectGroup>
@@ -41,7 +98,8 @@ function TranslationForm({ languages }: { languages: any }) {
                     ([key, value]: [string, any]) => (
                       <SelectItem
                         key={key}
-                        value={key}>
+                        value={key}
+                      >
                         {value.name}
                       </SelectItem>
                     )
@@ -54,6 +112,13 @@ function TranslationForm({ languages }: { languages: any }) {
               placeholder='Type your message here.'
               className='min-h-32 text-xl'
               name='input'
+              value={text}
+              onChange={(e) => {
+                setText(e.target.value);
+                if (!e.target.value) {
+                  setTranslatedText('');
+                }
+              }}
             />
           </div>
 
@@ -61,7 +126,9 @@ function TranslationForm({ languages }: { languages: any }) {
             <div className='flex items-center justify-between'>
               <Select
                 name='outputLanguage'
-                defaultValue='es'>
+                defaultValue='ar'
+                onValueChange={handleValueChange}
+              >
                 <SelectTrigger className='w-[280px] border-none text-blue-500 font-bold'>
                   <SelectValue placeholder='Select a language' />
                 </SelectTrigger>
@@ -73,7 +140,8 @@ function TranslationForm({ languages }: { languages: any }) {
                       ([key, value]: [string, any]) => (
                         <SelectItem
                           key={key}
-                          value={key}>
+                          value={key}
+                        >
                           {value.name}
                         </SelectItem>
                       )
@@ -84,15 +152,35 @@ function TranslationForm({ languages }: { languages: any }) {
             </div>
 
             <Textarea
-              className='min-h-32 text-xl bg-gray-50 border-none'
-              placeholder='Translation will appear here...'
+              className={`min-h-32 text-xl bg-gray-50 border-none text-black ${
+                to === 'ar' && translatedText && text ? 'text-right' : ''
+              }`}
+              style={{
+                opacity: '1 !important',
+                color: '#000000',
+                direction: to === 'ar' ? 'rtl' : 'ltr',
+              }}
+              placeholder={
+                to === 'ar'
+                  ? 'ستظهر الترجمة هنا...'
+                  : 'Translation will appear here...'
+              }
               name='output'
+              disabled
+              value={translatedText}
+              onChange={(e) => setTranslatedText(e.target.value)}
             />
           </div>
         </div>
 
         <div className='mt-5 flex justify-end'>
-          <SubmitButton />
+          <Button
+            type='submit'
+            disabled={loading}
+            className='bg-blue-500 hover:bg-blue-600 w-full lg:w-fit'
+          >
+            {loading ? 'Translating...' : 'Translate'}
+          </Button>
           <button
             type='submit'
             hidden
